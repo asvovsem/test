2ef2c1aaba28d4d369602c36b46460c964be6c7a
asvovsem <asvovsem@gmail.com>
2016-03-27 16:03:11 +0300

    def pos def main functionality

--- Snake.py
+++ Snake.py
@@ -1,3 +1,13 @@
-print("HELLO WORLD!")
-input()
-print("Say smth please!")
+from colorama import *
+
+def pos(x,y):
+    return '\x1b[' + str(y) + ';' + str(x) + 'H'
+def main():
+
+    print ("Typo your favourite x and y posses")
+    x1 = input()
+    y1 = input()
+    print (Fore.WHITE + pos( x1, y1) + "this")
+    input()
+if (__name__ == "__main__" ):
+    main()
